b84988d081864947dde408a5e6669e1ff4c54e91
Raphael Egeley <raphael.egeley@gmail.com>
2016-05-28 23:38:27 +0200

    Begin HMAC processing

--- client/client.py
+++ client/client.py
@@ -1,55 +1,75 @@
 #! /usr/bin/env python
 
 import argparse
+import hashlib
+import hmac
 import os
 import socket
 import ssl
 import sys
 
+def digest_file(filename, secret):
+    # Use oh SHA1 algorithm to hash file content
+    digester = hmac.new(secret, '', hashlib.sha1)
+    f = open(filename, 'rb')
+    try:
+        while True:
+            block = f.read(1024)
+            if not block:
+                break
+            digester.update(block)
+    finally:
+        f.close()
+    return digester.hexdigest()
+
 class Client:
-    def __init__(self, host, port, ca, cert, key):
+    def __init__(self, host, port, ca, cert, key, stock, filename):
         self.host = host
         self.port = port
+        self.stock = stock
+        self.filename = filename
         self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         self.context.verify_mode = ssl.CERT_REQUIRED
         self.context.check_hostname = True
         # Set CA location in order to check server certificates
-        self.context.load_verify_locations('/home/rapha/projets/3dtrust/ca_cert/minissl-ca.pem')
+        self.context.load_verify_locations(ca)
         try:
             # Load is own certificate and the related key
             # An SSLError is raised if the client private key doesn't
             # match with the client certificate.
-            self.context.load_cert_chain(
-                    '/home/rapha/projets/3dtrust/client/minissl-client.pem',
-                    '/home/rapha/projets/3dtrust/client/minissl-client.key.pem'
-            )
+            self.context.load_cert_chain(cert, key)
         except ssl.SSLError, (value, message):
             print message
             sys.exit(value)
 
-    def connect(self):
-        conn = self.context.wrap_socket(
-                socket.socket(socket.AF_INET),
-                server_hostname = 'minissl-SERVER'
-        )
+    def get_file(self, ssl_socket):
+        ssl_socket.send('GET ' + self.filename)
+        # Receive secret use for HMAC from server
+        secret = ssl_socket.recv(1024)
+        # Receive HMAC signature for the requested file
+        signature = ssl_socket.recv(1024)
+        # Debug
+        print signature
+        print digest_file('server/store/' + self.filename, secret)
 
+    def deal_with_server(self):
+        ssl_socket = self.context.wrap_socket(
+                socket.socket(socket.AF_INET),
+                #TODO change default hostname
+                server_hostname = 'minissl-SERVER'
+                )
         try:
-            conn.connect((self.host, self.port))
+            # Try to connect to the server
+            ssl_socket.connect((self.host, self.port))
             print 'connected to ' + self.host + ':' + str(self.port)
-            conn.send('GET filename')
-            data = conn.recv(1024)
-            print 'data received : ' + data
+            # Send a Get request in order to received the file
+            self.get_file(ssl_socket)
         except ssl.SSLError, (value, message):
-            print message
-            print 'Error dealing with server'
+            print "Error dealing with server :\r\n\t" + message
         finally:
             print 'Connexion with server closed'
-            conn.shutdown(socket.SHUT_RDWR)
-            conn.close()
-
-
-
-
+            ssl_socket.shutdown(socket.SHUT_RDWR)
+            ssl_socket.close()
 
 def def_path(path_file):
     path = os.path.dirname(sys.argv[0])
@@ -61,6 +81,7 @@ if __name__ == "__main__":
     ca = def_path('../ca_cert/minissl-ca.pem')
     cert = def_path('minissl-client.pem')
     key = def_path('minissl-client.key.pem')
+    stock = def_path('store/minissl-client.key.pem')
 
     parser = argparse.ArgumentParser(
             description = 'TLS Client - Send a GET filename request',
@@ -71,7 +92,10 @@ if __name__ == "__main__":
     parser.add_argument('--ca', default = ca, help = ' ')
     parser.add_argument('--cert', default = cert, help = ' ')
     parser.add_argument('--key', default = key, help = ' ')
+    parser.add_argument('--stock', default = stock, help = ' ')
+    parser.add_argument('--filename', default = 'toto', help = ' ')
     args = parser.parse_args()
-    c = Client(args.hostname, args.port, args.ca, args.cert, args.key)
-    c.connect()
+    c = Client(args.hostname, args.port, args.ca, args.cert,
+               args.key, args.stock, args.filename)
+    c.deal_with_server()
 
--- server/server.py
+++ server/server.py
@@ -1,18 +1,39 @@
 #! /usr/bin/env python
 
 import argparse
+import hashlib
+import hmac
 import os
 import socket
 import ssl
 import sys
 
+def digest_file(filename, secret):
+    # Use oh SHA1 algorithm to hash file content
+    digester = hmac.new(secret, '', hashlib.sha1)
+    f = open(filename, 'rb')
+    try:
+        while True:
+            block = f.read(1024)
+            if not block:
+                break
+            digester.update(block)
+    finally:
+        f.close()
+    return digester.hexdigest()
+
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, ca, cert, key, store):
         self.host = host
         self.port = port
+        self.store = store
+        if not os.path.isdir(store):
+            print "The store directory %s doesn't exist. Exiting" %store
+            sys.exit(1)
+
         self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         # Set CA location in order to check client certificates
-        self.context.load_verify_locations('/home/rapha/projets/3dtrust/ca_cert/minissl-ca.pem')
+        self.context.load_verify_locations(ca)
         # Require a certificate for each client
         self.context.verify_mode = ssl.CERT_REQUIRED
         # Register a callback function that will be called after
@@ -22,28 +43,41 @@ class Server:
             # Load is own certificate and the related key
             # An SSLError is raised if the server private key doesn't
             # match with his certificate.
-            self.context.load_cert_chain(
-                certfile = "/home/rapha/projets/3dtrust/server/minissl-server.pem",
-                keyfile = "/home/rapha/projets/3dtrust/server/minissl-server.key.pem"
-            )
+            self.context.load_cert_chain(cert, key)
         except ssl.SSLError, (value, message):
             print message
             sys.exit(value)
-        #TODO context.set_ciphers
+        self.context.set_ciphers('AES128')
 
     def verify_callback(self, ssl_socket, server_name, context):
-        print ssl_socket
-        print server_name
-        print context
+        print "New client HELLO received"
 
-    def deal_with_client(self, connstream):
-        data = connstream.recv(1024)
-        print "new client"
-        print 'data received : ' + data
-        # null data means the client is finished with us
-        if data == 'GET filename':
-            connstream.send("encrypted file")
-        # finished with client
+    def send_file(self, ssl_socket, filename):
+        print 'GET %s request received' %filename
+        # TODO verify filename (alphanumeric and present on server)
+        # We can admit here that the requested file is already encrypted
+        file_path = self.store + '/' + filename
+        if not os.path.isfile(file_path):
+            # The requested file is not found on the server
+            # TODO Send error message to client
+            print 'File doesn\'t exist'
+            return
+        # Generate random bytes secret used for HMAC
+        secret = os.urandom(64)
+        # Send secret to client
+        ssl_socket.send(secret)
+        # Send HMAC signature for this file
+        ssl_socket.send(digest_file(file_path, secret));
+        print digest_file(file_path, secret);
+
+    def deal_with_client(self, ssl_socket):
+        request = ssl_socket.recv(1024).split()
+        # Treate all differents request here
+        # For this example only GET "filename" request are handle
+        if request[0] == 'GET':
+            self.send_file(ssl_socket, request[1])
+        # Null data means the client is finished with us
+        # We have finished with this client now
 
     def run(self):
         bindsocket = socket.socket()
@@ -51,9 +85,11 @@ class Server:
         bindsocket.listen(16)
 
         while True:
+            # Waiting for clients here
             newsocket, fromaddr = bindsocket.accept()
             try:
-                connstream = self.context.wrap_socket(newsocket,
+                # When a client is connected : begin a tls session
+                ssl_socket = self.context.wrap_socket(newsocket,
                                                       server_side = True)
             except ssl.SSLError, (value, message):
                 print message
@@ -61,14 +97,14 @@ class Server:
                 continue
 
             try:
-                self.deal_with_client(connstream)
+                # When the TLS handshake has been done : listen client request
+                self.deal_with_client(ssl_socket)
             except ssl.SSLError, (value, message):
-                print message
-                print 'Error dealing with client'
+                print "Error dealing with client :\r\n\t" + message
             finally:
                 print 'Connexion with client closed'
-                connstream.shutdown(socket.SHUT_RDWR)
-                connstream.close()
+                ssl_socket.shutdown(socket.SHUT_RDWR)
+                ssl_socket.close()
 
 def def_path(path_file):
     path = os.path.dirname(sys.argv[0])
@@ -80,6 +116,7 @@ if __name__ == "__main__":
     ca = def_path('../ca_cert/minissl-ca.pem')
     cert = def_path('minissl-server.pem')
     key = def_path('minissl-server.key.pem')
+    store = def_path('store')
 
     parser = argparse.ArgumentParser(
             description = 'TLS Server - Respond on GET filename request',
@@ -90,7 +127,9 @@ if __name__ == "__main__":
     parser.add_argument('--ca', default = ca, help = ' ')
     parser.add_argument('--cert', default = cert, help = ' ')
     parser.add_argument('--key', default = key, help = ' ')
+    parser.add_argument('--store', default = store, help = ' ')
     args = parser.parse_args()
-    s = Server(args.hostname, args.port)
+    s = Server(args.hostname, args.port, args.ca,
+               args.cert, args.key, args.store)
     s.run()
 
